dbd9ed33d5140630b1a7b9cad7c7c90019f811da
suhail12222 <muhmmadasifmalik786@outlook.com>
2024-07-05 21:51:38 +0530

    Update Job.java

--- labour_Connect/model/Job.java
+++ labour_Connect/model/Job.java
@@ -15,6 +15,48 @@ public class Job {
     private String location;
     private double payment;
     private Long employerId;
+public void SteId(int id){
+    this.id=id;
+    System.out.println("this is the programm for labour ");
+}
+    public Long getId(){
+        return id;
+    }
+    public void setTitel(String titel){
+        this.title=title;
+        System.out.println(title)
+    }
+    public String getTitel(){
+        return title;
+    }
+public void setDescription(String description){
+    this.description=discription;
+    System.out.println(description);
+}
+    public String getDescription(){
+        retun description;
+    }
+    public void setLocation(String location);
+        this.location=location;
+System.out.println("this is the location of the employee :-"+location);
+    }
+    public String getLocation(){
+        return location;
+    }
+public void setPayment(double payment){
+    this.payment=payment;
+    System.ot.println(payment);
 
-    // Getters and setters
+}
+public double getPayment(){
+    return payment;
+}
+public void setEmployerId(Long employerId){
+    this.employerId=employerId;
+    System.out.println(employerId+"id of the");
+
+}
+    public long getEmployerId(){
+    return employerId;
+    }
 }
